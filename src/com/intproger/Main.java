a88437fc4147f675d1cf56a5c9a305ce29283d01
Ismar <ismar110501@gmail.com>
2023-09-01 18:39:44 +0600

    lesson2

--- src/com/intproger/Main.java
+++ src/com/intproger/Main.java
@@ -2,14 +2,35 @@ package com.intproger;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-            \n для переноса строки
-            \t таб в строке
-            \(любой символ для добавление)
-        */
 
-        System.out.println("Hello\nWorld!");
+        System.out.println("Переменные и типы данных");
+        byte age = 45;
 
 
     }
 }
+    /* переменные
+    числа:
+            byte: от -128 до 127
+            short: от от -32768 до 32767
+            int: много
+            long: очень много
+            float: 1.1 после прописание добавить "f" 12.134f
+            double: в 2 раза больше после точки и не обьязательно "f" в конце
+
+    текст:
+            char: только одинарные ковычки 'слова'
+            String: обьязательно "слова"
+
+    значение:
+            Boolean: false or true
+     */
+
+
+//    int age = 18;
+//
+//        System.out.print("Возраст: " + age + ".");
+//
+//        age = 18;
+//
+//        System.out.print(age);
