d8bfc4cff3787ec43ea4369c74f4a00cd55cc7d8
Ismar <ismar110501@gmail.com>
2023-09-01 18:22:51 +0600

    lesson1

---
+++ src/com/intproger/Main.java
@@ -0,0 +1,15 @@
+package com.intproger;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            \n для переноса строки
+            \t таб в строке
+            \(любой символ для добавление)
+        */
+
+        System.out.println("Hello\nWorld!");
+
+
+    }
+}
