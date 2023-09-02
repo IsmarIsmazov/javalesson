9f2515caaa15f5667e004c53aef8e8d94378d98d
Ismar <ismar110501@gmail.com>
2023-09-02 12:39:35 +0600

    lesson3

--- src/com/intproger/Main.java
+++ src/com/intproger/Main.java
@@ -1,36 +1,31 @@
 package com.intproger;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Переменные и типы данных");
-        byte age = 45;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите первое число:");
+        float num1 = scan.nextInt();
+        System.out.print("Введите второе число:");
+        float num2 = scan.nextFloat();
+        float resplus = num1 + num2;
+        float resminus = num1 - num2;
+        float resdivision = num1 / num2;
+        float resmultiplication = num1 * num2;
+        System.out.println("Результаты: \n" +
+                resplus + "\n" + resminus +
+                "\n" + resdivision +
+                "\n" + resmultiplication);
 
 
     }
 }
-    /* переменные
-    числа:
-            byte: от -128 до 127
-            short: от от -32768 до 32767
-            int: много
-            long: очень много
-            float: 1.1 после прописание добавить "f" 12.134f
-            double: в 2 раза больше после точки и не обьязательно "f" в конце
-
-    текст:
-            char: только одинарные ковычки 'слова'
-            String: обьязательно "слова"
-
-    значение:
-            Boolean: false or true
-     */
-
-
-//    int age = 18;
-//
-//        System.out.print("Возраст: " + age + ".");
-//
-//        age = 18;
-//
-//        System.out.print(age);
+//        System.out.print("Введите свое имя ");
+//                String username = scan.nextLine();
+//                System.out.print("Введите свой возраст ");
+//                int age = scan.nextInt();
+//                age += 1;
+//                System.out.println("Здраствуйте " + username + "!\n" +
+//                "Вам через год будет: " + age );
