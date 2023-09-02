780382e291981cde98a33e36d6c09151c45ae0c3
Ismar <ismar110501@gmail.com>
2023-09-02 19:11:51 +0600

    lesson3.5

--- src/com/intproger/Main.java
+++ src/com/intproger/Main.java
@@ -5,27 +5,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите первое число:");
-        float num1 = scan.nextInt();
-        System.out.print("Введите второе число:");
-        float num2 = scan.nextFloat();
-        float resplus = num1 + num2;
-        float resminus = num1 - num2;
-        float resdivision = num1 / num2;
-        float resmultiplication = num1 * num2;
-        System.out.println("Результаты: \n" +
-                resplus + "\n" + resminus +
-                "\n" + resdivision +
-                "\n" + resmultiplication);
 
+
+        int a = 15, b = 10;
+        char sym1 = 'a', sym2 = 'A';
+        boolean IsHasCar = false;
+        if (!IsHasCar) {
+            System.out.print("да ");
+        } else if (IsHasCar) {
+            System.out.printf("Нет");
+        } else {
+            System.out.printf("нипон");
+
+        }
+        if (a > b) {
+            System.out.print("Всё верно ");
+        } else {
+            System.out.print("Не верно");
+        }
+        if (sym1 == sym2) {
+            System.out.print("Нет, не верно");
+        } else {
+            System.out.print("Не верно");
+        }
 
     }
 }
-//        System.out.print("Введите свое имя ");
-//                String username = scan.nextLine();
-//                System.out.print("Введите свой возраст ");
-//                int age = scan.nextInt();
-//                age += 1;
-//                System.out.println("Здраствуйте " + username + "!\n" +
-//                "Вам через год будет: " + age );
