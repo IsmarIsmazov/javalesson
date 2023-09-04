11daa811b7d94fcd51f490183fad5d96bc9f1784
Ismar <ismar110501@gmail.com>
2023-09-04 11:39:31 +0600

    lesson4

--- src/com/intproger/Main.java
+++ src/com/intproger/Main.java
@@ -5,29 +5,62 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("напишите первое число: ");
+        int num1 = scanner.nextInt();
+        System.out.print("напишите второе число: ");
+        int num2 = scanner.nextInt();
+        int res;
+        System.out.print("напишите действие: ");
+        String result = scanner.nextLine();
+        result = scanner.nextLine();
 
 
-        int a = 15, b = 10;
-        char sym1 = 'a', sym2 = 'A';
-        boolean IsHasCar = false;
-        if (!IsHasCar) {
-            System.out.print("да ");
-        } else if (IsHasCar) {
-            System.out.printf("Нет");
-        } else {
-            System.out.printf("нипон");
+        switch (result) {
+            case "+":
+                res = num1 + num2;
+                System.out.println("Результат " + res);
+                break;
+            case "-":
+                res = num1 - num2;
+                System.out.println("Результат " + res);
+                break;
+            case "/":
+                if (num2 == 0 || num1 == 0)
+                    System.out.println("на ноль делить нельзя");
+                else {
+                    res = num1 / num2;
+                    System.out.println("Результат " + res);
+                }
+                break;
+            case "*":
+                res = num1 * num2;
+                System.out.println("Результат " + res);
+                break;
+            default:
+                System.out.println("Введите правильно");
+        }
 
-        }
-        if (a > b) {
-            System.out.print("Всё верно ");
-        } else {
-            System.out.print("Не верно");
-        }
-        if (sym1 == sym2) {
-            System.out.print("Нет, не верно");
-        } else {
-            System.out.print("Не верно");
-        }
 
     }
 }
+//    int num = scanner.nextInt();
+//        switch (num) {
+//                case 1:
+//                System.out.println("number in one");
+//                break;
+//                case 2:
+//                System.out.println("number in two");
+//                break;
+//                case 3:
+//                System.out.println("number in three");
+//                break;
+//                case 4:
+//                System.out.println("number in four");
+//                break;
+//                case 5:
+//                System.out.println("number in five");
+//                break;
+//default:
+//        System.out.println("не правильно");
+//        }
